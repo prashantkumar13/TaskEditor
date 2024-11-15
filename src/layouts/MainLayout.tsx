be06203fb9c28892c81c7e1e1ff2ab95e744260c
Prashant Kumar <prashantofficial1304@gmail.com>
2024-11-15 19:41:06 +0530

    some changes

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import { BottomNav, ProfileSidebar } from "../components";
+import { BottomNav } from "../components";
 
 interface MainLayoutProps {
   children: ReactNode;
